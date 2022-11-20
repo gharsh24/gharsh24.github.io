d2ec081ead8f9e700a91cb05ec29fdd9ff32b3f6
Harsh Goyal <harsh.sg360@gmail.com>
2022-11-20 17:04:52 +0530

    Add all the files to repository

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6 
+
+const bodyParser = require("body-parser");
+const express=require("express");
+const ejs=require('ejs');
+const app=express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+app.listen(3000,function(req,res){
+    console.log("Server started at port 3000");
+});
